c9ebd835dfcc7a6b6e747ab0d92b94492c00d04f
unknown <stephanrossi@gmail.com>
2021-12-08 13:49:01 -0300

    1

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from './../services/CreateCategoryService';
+
+export const CategoriesRoutes = Router()
+
+const categoriesRepository = new CategoriesRepository()
+
+CategoriesRoutes.post('/', (req, res) => {
+    const { name, description } = req.body
+
+    const createCategoryService = new CreateCategoryService(categoriesRepository)
+
+    createCategoryService.execute({ name, description })
+
+    return res.status(201).send()
+})
+
+CategoriesRoutes.get('/', (req, res) => {
+    const categories = categoriesRepository.list()
+
+    return res.status(200).json({
+        error: false,
+        data: categories
+    })
+})
---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,23 @@
+import { CategoriesRepository } from './../repositories/CategoriesRepository';
+
+
+interface iRequest {
+    name: string
+    description: string
+}
+
+export class CreateCategoryService {
+    constructor(private CategoriesRepository: CategoriesRepository) {
+
+    }
+    execute({ name, description }: iRequest): void {
+
+        const categoryAlreadyExists = this.CategoriesRepository.findByName(name)
+
+        if (categoryAlreadyExists) {
+            throw new Error("Categoria já existe");
+        }
+
+        this.CategoriesRepository.create({ name, description })
+    }
+}
